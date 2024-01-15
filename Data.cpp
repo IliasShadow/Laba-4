d60d736d7cf1238793106cc3d4a27b513cc00444
IliasShadow <145995986+IliasShadow@users.noreply.github.com>
2024-01-16 00:42:57 +1000

    Update Data.cpp

--- Data.cpp
+++ Data.cpp
@@ -2,79 +2,39 @@
 #include <cstring>
 using namespace std;
 
-class Data{
-	public:
-	bool isOurEra;
-	int year;
-	int month;
-	int day;
-	int hour;
-	int minute;
-	int second;
-	public:
-	Data(){
-	this->isOurEra=1;
-	this->year=1980;
-	this->month=1;
-	this->day=1;
-	this->hour=0;
-	this->minute=0;
-	this->second=0;
-	}
-	 bool visok(int year){
-        return (((year%4==0)&&(year%100!=0))||(year%400==0));
-		}
-	void norm(){
-		minute+=second/60;
-		second%=60;
-		hour+=minute/60;
-		minute%=60;
-		while (second<0){
-            second+=60;
-            minute--;
-        }
-        while (minute<0){
-            minute+=60;
-            hour--;
-        }
-        while (hour<0){
-            hour+= 24;
-            day--;
-        }
-
-		int daysofMonth[]={0,31,28,31,30,31,30,31,31,30,31,30,31};
-
-		 while (day<1){
-            month--;
-            if (month<1){
-                month=12;
-                year--;
-            }
-            int days=daysofMonth[month];
-            if (month==2 && visok(year)){
-                days++;
-            }
-            day+=days;
-        }
-        while (month>12){
-            month-=12;
-            year++;
-        }
-        if (year<=0){
-            year= 1-year;
-            isOurEra=0;
-        }else{isOurEra=1;}
+class Date {
+private:
+    bool isOurEra;
+    int year;
+    int month;
+    int day;
+    int hour;
+    int minute;
+    int second;
+public:
+    bool isLeapYear(int year) {
+        return ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
     }
-	Data(const Data& dat){
-		this->isOurEra=dat.isOurEra;
-	    this->year=dat.year;
-	    this->month=dat.month;
-	    this->day=dat.day;
-	    this->hour=dat.hour;
-	    this->minute=dat.minute;
-	    this->second=dat.second;
-	}
-	Data(int year1,int month1,int day1,int hour1,int minute1,int second1,bool isOurEra1){
+
+    int daysInMonth(int month, int year) {
+        int daysOfMonth[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (month == 2 && isLeapYear(year)) {
+            return 29;
+        }
+        return daysOfMonth[month];
+    }
+
+
+    Date() {
+        this->isOurEra = true;
+        this->year = 1960;
+        this->month = 1;
+        this->day = 1;
+        this->hour = 0;
+        this->minute = 0;
+        this->second = 0;
+    }
+    Date(int year1,int month1,int day1,int hour1,int minute1,int second1,bool isOurEra1){
 		this->isOurEra=isOurEra1;
 	    this->year=year1;
 	    this->month=month1;
@@ -83,171 +43,252 @@ class Data{
 	    this->minute=minute1;
 	    this->second=second1;
 	}
-	Data& operator =(const Data& dat){
-		this->isOurEra=dat.isOurEra;
-	    this->year=dat.year;
-	    this->month=dat.month;
-	    this->day=dat.day;
-	    this->hour=dat.hour;
-	    this->minute=dat.minute;
-	    this->second=dat.second;
-		return *this;
-	}
-	Data add(int year1,int month1,int day1,int hour1,int minute1,int second1){
-		Data r=*this;
-	    r.year+=year1;
-	    r.month+=month1;
-	    r.day+=day1;
-	    r.hour+=hour1;
-	    r.minute+=minute1;
-	    r.second+=second1;
-		r.norm();
-		return r;
-	}
-	Data subtract(int year1,int month1,int day1,int hour1,int minute1,int second1){
-		Data r=*this;
-	    r.year-=year1;
-	    r.month-=month1;
-	    r.day-=day1;
-	    r.hour-=hour1;
-	    r.minute-=minute1;
-	    r.second-=second1;
-		r.norm();
-		return r;
-	}
+    Date add(int year1, int month1, int day1, int hour1, int minute1, int second1) {
+        Date result = *this;
+        result.year += year1;
+        result.month += month1;
+        result.day += day1;
+        result.hour += hour1;
+        result.minute += minute1;
+        result.second += second1;
+        result.norm();
+        return result;
+    }
 
-	bool operator ==(const Data& dat){
-		Data r;
-		if ((isOurEra==dat.isOurEra)&& (year==dat.year) && (month==dat.month)&& (day==dat.day)&& (hour==dat.hour)&& (minute==dat.minute)&& (second==dat.second)) return 1;
-	}
-	bool operator <=(const Data& dat){
-		return  ((isOurEra<=dat.isOurEra)&& (year<=dat.year) && (month<=dat.month)&& (day<=dat.day)&& (hour<=dat.hour)&& (minute<=dat.minute)&& (second<=dat.second));
-	}
-	bool operator <(const Data& dat){
-		if (isOurEra!=dat.isOurEra){
-	return isOurEra<dat.isOurEra;
-	}else{
-	if (year!=dat.year){
-	return year<dat.year;
-	}else{
-	if (month!=dat.month){
-	return month<dat.month;
-	}else{
-	if (day!=dat.day){
-	return day<dat.day;
-	}else{
-	if (hour!=dat.hour){
-	return hour<dat.hour;
-	}else{
-	if (minute!=dat.minute){
-	return minute<dat.minute;
-	}else{
-	if (second>dat.second){
-	return second<dat.second;
-	}else return 0;
-	}
-	}
-	}
-	}
-	}
-	}
-		}
-	bool operator >(const Data& dat){
-		if (isOurEra!=dat.isOurEra){
-	return isOurEra>dat.isOurEra;
-	}else{
-	if (year!=dat.year){
-	return year>dat.year;
-	}else{
-	if (month!=dat.month){
-	return month>dat.month;
-	}else{
-	if (day!=dat.day){
-	return day>dat.day;
-	}else{
-	if (hour!=dat.hour){
-	return hour>dat.hour;
-	}else{
-	if (minute!=dat.minute){
-	return minute>dat.minute;
-	}else{
-	if (second>dat.second){
-	return second>dat.second;
-	}else return 0;
-	}
-	}
-	}
-	}
-	}
-	}
-		}
-	bool operator >=(const Data& dat){
-		return ((isOurEra>=dat.isOurEra)&& (year>=dat.year) && (month>=dat.month)&& (day>=dat.day)&& (hour>=dat.hour)&& (minute>=dat.minute)&& (second>=dat.second));
-	}
+    Date subtract(int year1, int month1, int day1, int hour1, int minute1, int second1) {
+        Date result = *this;
+        result.year -= year1;
+        if (result.year<=0){
+            result.year=1-result.year;
+        result.isOurEra=0;
+        result.month -= month1;
+        result.day -= day1;
+        result.hour -= hour1;
+        result.minute -= minute1;
+        result.second -= second1;
+        }else{
+        result.month -= month1;
+        result.day -= day1;
+        result.hour -= hour1;
+        result.minute -= minute1;
+        result.second -= second1;
+        }
+        result.norm();
+        return result;
+    }
 
-	Data& operator +=(const Data& dat){
-	    year+=dat.year;
-	    month+=dat.month;
-	    day+=dat.day;
-	    hour+=dat.hour;
-	    minute+=dat.minute;
-	    second+=dat.second;
-		norm();
-		return *this;
-	}
-	Data& operator -=(const Data& dat){
-		year-=dat.year;
-	    month-=dat.month;
-	    day-=dat.day;
-	    hour-=dat.hour;
-	    minute-=dat.minute;
-	    second-=dat.second;
-		norm();
-		return *this;
+    void norm() {
+        minute += second / 60;
+        second %= 60;
+        if (second<0){
+        second+=60;
+        minute--;
 
-	}
-	Data operator +(const Data& dat){
-		Data r;
-		int year1=dat.year;
-	    int month1=dat.month;
-	    int day1=dat.day;
-	    int hour1=dat.hour;
-	    int minute1=dat.minute;
-	    int second1=dat.second;
-		r=r.add(year1,month1,day1,hour1,minute1,second1);
-		return r;
-	}
-	Data operator -(const Data& dat){
-		Data r;
-		int year1=dat.year;
-	    int month1=dat.month;
-	    int day1=dat.day;
-	    int hour1=dat.hour;
-	    int minute1=dat.minute;
-	    int second1=dat.second;
-		r=r.subtract(year1,month1,day1,hour1,minute1,second1);
-		return r;
-	}
-	friend ostream& operator<<(ostream& os, const Data& dat){
+        }
+
+        hour += minute / 60;
+        minute %= 60;
+        if (second<0){
+        minute+=60;
+        hour--;
+
+        }
+        day+=hour/24;
+        hour%=24;
+        if(hour < 0) {
+            hour += 24;
+            day--;
+        }
+
+        while (day < 1) {
+            month--;
+            if (month < 1) {
+                month = 12;
+                year--;
+            }
+            day += daysInMonth(month, year);
+        }
+
+        while (day > daysInMonth(month, year)) {
+            day -= daysInMonth(month, year);
+            month++;
+            if (month > 12) {
+                month = 1;
+                year++;
+            }
+        }
+
+        if(month < 1) {
+            month += 12;
+            year--;
+        }
+
+        if (year <= 0) {
+            year = 1 - year;
+            isOurEra = false;
+        } else {
+            isOurEra = true;
+        }
+    }
+
+    friend ostream& operator<<(ostream& os, const Date& dat) {
         os << dat.year << " " << dat.month << " " << dat.day << " " << dat.hour << " " << dat.minute << " " << dat.second;
         if (dat.isOurEra) os << " нашей эры";
         else os << " до нашей эры";
         return os;
-	}
+    }
 
+    Date& operator =(const Date& dat) {
+        this->isOurEra = dat.isOurEra;
+        this->year = dat.year;
+        this->month = dat.month;
+        this->day = dat.day;
+        this->hour = dat.hour;
+        this->minute = dat.minute;
+        this->second = dat.second;
+        return *this;
+    }
+
+    Date operator +(const Date& dat) {
+        Date result=*this;
+        int year1 = dat.year;
+
+        int month1 = dat.month;
+        int day1 = dat.day;
+        int hour1 = dat.hour;
+        int minute1 = dat.minute;
+        int second1 = dat.second;
+        result=result.add(year1, month1, day1, hour1, minute1, second1);
+        return result;
+    }
+
+    Date operator -(const Date& dat) {
+        Date result=*this;
+        int year1 = dat.year;
+        int month1 = dat.month;
+        int day1 = dat.day;
+        int hour1 = dat.hour;
+        int minute1 = dat.minute;
+        int second1 = dat.second;
+        result=result.subtract(year1, month1, day1, hour1, minute1, second1);
+        return result;
+    }
+
+    bool operator ==(const Date& dat) {
+        return ((isOurEra == dat.isOurEra) && (year == dat.year) && (month == dat.month) && (day == dat.day) &&
+                (hour == dat.hour) && (minute == dat.minute) && (second == dat.second));
+    }
+
+    bool operator <=(const Date& dat) {
+        return ((isOurEra <= dat.isOurEra) && (year <= dat.year) && (month <= dat.month) && (day <= dat.day) &&
+                (hour <= dat.hour) && (minute <= dat.minute) && (second <= dat.second));
+    }
+
+    bool operator <(const Date& dat) {
+        if (isOurEra != dat.isOurEra) {
+            return isOurEra < dat.isOurEra;
+        } else {
+            if (year != dat.year) {
+                return year < dat.year;
+            } else {
+                if (month != dat.month) {
+                    return month < dat.month;
+                } else {
+                    if (day != dat.day) {
+                        return day < dat.day;
+                    } else {
+                        if (hour != dat.hour) {
+                            return hour < dat.hour;
+                        } else {
+                            if (minute != dat.minute) {
+                                return minute < dat.minute;
+                            } else {
+                                return second < dat.second;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    bool operator >(const Date& dat) {
+        if (isOurEra != dat.isOurEra) {
+            return isOurEra > dat.isOurEra;
+        } else {
+            if (year != dat.year) {
+                return year > dat.year;
+            } else {
+                if (month != dat.month) {
+                    return month > dat.month;
+                } else {
+                    if (day != dat.day) {
+                        return day > dat.day;
+                    } else {
+                        if (hour != dat.hour) {
+                            return hour > dat.hour;
+                        } else {
+                            if (minute != dat.minute) {
+                                return minute > dat.minute;
+                            } else {
+                                return second > dat.second;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    bool operator >=(const Date& dat) {
+        return ((isOurEra >= dat.isOurEra) && (year >= dat.year) && (month >= dat.month) && (day >= dat.day) &&
+                (hour >= dat.hour) && (minute >= dat.minute) && (second >= dat.second));
+    }
+
+    Date& operator +=(const Date& dat) {
+        if (isOurEra == dat.isOurEra) {
+            year += dat.year;
+            month += dat.month;
+            day += dat.day;
+            hour += dat.hour;
+            minute += dat.minute;
+            second += dat.second;
+            norm();
+        } else {
+            year += dat.year;
+            month += dat.month;
+            day += dat.day;
+            hour += dat.hour;
+            minute += dat.minute;
+            second += dat.second;
+            norm();
+        }
+        return *this;
+    }
+
+    Date& operator -=(const Date& dat) {
+        year -= dat.year;
+        month -= dat.month;
+        day -= dat.day;
+        hour -= dat.hour;
+        minute -= dat.minute;
+        second -= dat.second;
+        norm();
+        return *this;
+    }
 };
-int main(){
-	setlocale(0,"");
-	int main() {
-    Data d1;
+
+int main() {
+    Date d1;
+    setlocale(0, "");
     cout << d1  << endl;
-    Data d2(2060, 1, 1, 0, 0, 0, true);
+    Date d2(2060, 1, 1, 0, 0, 0, true);
     cout << (d1 > d2) << endl;
     cout << (d1 < d2) << endl;
     cout << (d1 == d2) << endl;
-    Data d3 = d2.substruct(2060, 1, 1, 0, 0, 0);
-    Data d4(d3 + d2 - d1);
-    Data d5(d4);
+    Date d3 = d2.subtract(2060, 1, 1, 0, 0, 0);
+    Date d4(d3 + d2 - d1);
+    Date d5(d4);
     d4 += d5 - d4.add(40, 1, 1, 0, 0, 0);
 
     cout << d3 << endl;
@@ -255,6 +296,4 @@ int main(){
     cout << d5 << endl;
 
     return 0;
-}
-
 }
